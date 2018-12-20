0172a4c28ed269d3d955a095805ac456e3e67258
Krislin <krislin.alas@gmail.com>
2018-12-20 20:58:02 +0200

    Added converter

--- src/main/java/com/sda/project/bookinglist/repository/NewsletterRepository.java
+++ src/main/java/com/sda/project/bookinglist/repository/NewsletterRepository.java
@@ -4,9 +4,12 @@ import com.sda.project.bookinglist.entity.NewsletterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface NewsletterRepository extends JpaRepository<NewsletterEntity, Long> {
 
+    Optional<NewsletterEntity> findByEmail(final String email);
 
 }
