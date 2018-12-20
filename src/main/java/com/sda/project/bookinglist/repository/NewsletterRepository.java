c0f7a8bf11b330e3cf5a5161b04514e337fbb20d
Jurgen P <prualjurgen@gmail.com>
2018-12-20 18:58:08 +0200

    commit for securit

--- src/main/java/com/sda/project/bookinglist/repository/NewsletterRepository.java
+++ src/main/java/com/sda/project/bookinglist/repository/NewsletterRepository.java
@@ -1,14 +1,12 @@
 package com.sda.project.bookinglist.repository;
 
 import com.sda.project.bookinglist.entity.NewsletterEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface NewsletterRepository extends CrudRepository<NewsletterEntity, Long> {
-
-
+public interface NewsletterRepository extends JpaRepository<NewsletterEntity, Long> {
 
 
 }
