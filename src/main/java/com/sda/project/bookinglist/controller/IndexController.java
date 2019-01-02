44598ea2772ce60a0b5b36fe30eaaaa2072052a5
Jurgen P <prualjurgen@gmail.com>
2019-01-02 20:07:50 +0200

    got some front end :)

---
+++ src/main/java/com/sda/project/bookinglist/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.sda.project.bookinglist.controller;
+
+import com.sda.project.bookinglist.model.NewsletterModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView indexPage() {
+        return new ModelAndView("index");
+    }
+}
